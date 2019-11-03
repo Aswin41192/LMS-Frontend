6d04badb24b97473c50c10d3e347b93e321a39c8
Aswin41192 <aswins41192@gmail.com>
2019-11-03 09:45:06 +0530

    Multiple file uploads implemented

--- src/app/course-document/course-document.component.ts
+++ src/app/course-document/course-document.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 import { DataTranserService } from '../services/data.transfer.service';
 import { CourseDocument } from '../model/CourseDocument';
 import { Course } from '../model/Course';
@@ -13,9 +13,12 @@ import { NgxSpinnerService } from 'ngx-spinner';
 export class CourseDocumentComponent implements OnInit {
   course: Course;
   courseDocuments: CourseDocument[] = [];
-  file: File = null;
+  files: File[] = [];
+
+  @ViewChild('uploadedFile', { static: false })
+  private host: ElementRef;
   constructor(private dataTransferService: DataTranserService, private courseService: CourseService,
-              private spinner: NgxSpinnerService) { }
+    private spinner: NgxSpinnerService) { }
 
   ngOnInit() {
     this.course = this.dataTransferService.getData();
@@ -31,8 +34,8 @@ export class CourseDocumentComponent implements OnInit {
     if (reply) {
       this.spinner.show();
       const payload = {
-        "_id" : this.course._id,
-        "documentId" : documentId
+        "_id": this.course._id,
+        "documentId": documentId
       };
       this.courseService.deleteCourse(payload).subscribe(res => {
         console.log('response', res);
@@ -40,7 +43,7 @@ export class CourseDocumentComponent implements OnInit {
           alert(res.response.message);
           console.log('Courses', this.course);
           const index = this.course.courseDocuments.
-                        findIndex(courseDocument => courseDocument._id === documentId);
+            findIndex(courseDocument => courseDocument._id === documentId);
           this.course.courseDocuments.splice(index, 1);
         } else {
           alert(res.response.message);
@@ -51,14 +54,19 @@ export class CourseDocumentComponent implements OnInit {
   }
 
   getUploadedFile(files: FileList) {
-    console.log('Uploaded File ', files.item(0));
-    this.file = files.item(0);
+    console.log('Uploaded File ', files);
+    for (let i = 0; i < files.length; i++) {
+      this.files.push(files.item(i));
+    }
   }
-
-  uploadDocument() {
+  uploadDocument(uploadedFile) {
+    console.log('uploadedFile', uploadedFile.reset);
+    uploadedFile.reset();
     this.spinner.show();
-    const formData: FormData = new  FormData();
-    formData.append('courseDocument', this.file);
+    const formData: FormData = new FormData();
+    for (let i = 0; i<this.files.length; i++) {
+      formData.append('courseDocument', this.files[i]);
+    }
     formData.append('_id', this.course._id);
     console.log('Form Data', formData);
     this.courseService.uploadCourseDocument(formData).subscribe(res => {
@@ -68,8 +76,11 @@ export class CourseDocumentComponent implements OnInit {
         this.course.courseDocuments = course.courseDocuments;
         console.log('added', this.course.courseDocuments);
       } else {
-          alert('Error while uploading document');
+        alert('Error while uploading document');
       }
+      console.log('host', this.host.nativeElement.value);
+      this.host.nativeElement.value = '';
+      this.files = [];
       this.spinner.hide();
     });
   }
